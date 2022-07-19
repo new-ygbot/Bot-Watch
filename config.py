267867821c0c069c843670a1e6a518d86621cc7d
Diego Alberto <106711900+RockstarDevrloperCuba@users.noreply.github.com>
2022-07-18 22:33:52 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+apiid = 16536294
+
+apihash = "0c780f6db3124fafbdc3dcde437e6f21"
+
+token = "5511880988:AAGm5-pa-cekIfus2cQjz3W62bARE6zVmcE"
+
+adminbot = "rockstar984"
+admin_id = 1180073584
+
+useraccept = []
