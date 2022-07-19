18e1e49c0c7deff7c80ae3a98561fa946092bbe3
Diego Alberto <106711900+RockstarDevrloperCuba@users.noreply.github.com>
2022-07-19 08:21:55 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ apiid = 16536294
 
 apihash = "0c780f6db3124fafbdc3dcde437e6f21"
 
-token = "5511880988:AAGm5-pa-cekIfus2cQjz3W62bARE6zVmcE"
+token = "5513331143:AAEuIifc1VvCEANfrRbWsfDDkkdc6zfrFbQ"
 
 adminbot = "rockstar984"
 admin_id = 1180073584
